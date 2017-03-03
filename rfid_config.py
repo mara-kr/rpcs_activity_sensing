59b1162e9fb068610585fd286da01894a55c5fff
Neil Ryan <soctar@gmail.com>
2017-03-03 20:01:26 +0000

    Added TODOs

--- rfid_config.py
+++ rfid_config.py
@@ -5,10 +5,21 @@
 # LEN(1), ADDR(1), COMMAND(1), DATA(var), CRCLSB(1), CRCMSB(1)
 # Neil Ryan <nryan@andrew.cmu.edu>
 
+# Functions to send each type of command, waits for response in function
+# Include parsing of status/len/etc
+# Get Work command 0x36
+# Set work command 0x35
+# Acoustio-optic 0x33
+# Set scan time 0x25
+# Set power (with response)
+# Set address 0x24
+# Get reader info 0x21
+# Inventory single 6b 0x50
+# inventory multiple 0x51
+
 import serial
 
 TAG_LENGTH = 18
-POLYNOMAIL = 0x8408
 
 
 def crc(data):
@@ -21,11 +32,12 @@ def crc(data):
             else:
                 val = val >> 1
     return val
+
     
 # Command - int
 # Data - List of bytes
 def sendCommand(ser, command, data):
-    msg_data = [len(data) + 4,0xFF]
+    msg_data = [len(data) + 4,0x0]
     msg_data.append(command)
     msg_data += data
     crc_val = crc(msg_data)
@@ -38,6 +50,7 @@ def sendCommand(ser, command, data):
 
     ser.write(data_str)
 
+
 ser = serial.Serial(
     port='/dev/ttyUSB0',
     baudrate='57600',
@@ -47,11 +60,20 @@ ser = serial.Serial(
     timeout=1
 )
 
-sendCommand(ser, 0x33, [0, 0xFF, 0xFF])
-print('\n')
-a = 1
+# Sets the power level of the reader
+def setPower(ser, power_level):
+    sendCommand(ser, 0x2f, [power_level])
+
+
+
+#sendCommand(ser, 0x35, [2, 0x2, 0x4, 0x0, 0x12, 0x0])
+sendCommand(ser, 0x36, []) #Gets work mode
+#sendCommand(ser, 0x21, [])
+
+print('')
+a = 2
 while (a > 0):
-    x = ser.readline(TAG_LENGTH)
+    x = ser.readline(32)
     if x:
         x = x.encode('hex')
         s = ""
