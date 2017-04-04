db82147f8889d9a7e6b0bbd19a040468a7b1ac79
Neil Ryan <soctar@gmail.com>
2017-04-04 03:04:21 +0000

    Some bugfixes, sends valid commands but commands don't necessairly do the thing we think they do - i.e. turn off beeping doesn turn off beeping

--- rfid_config.py
+++ rfid_config.py
@@ -8,35 +8,35 @@
 import serial
 
 TAG_LENGTH = 18
-MSG_FORMATTER = "%c%c%c%s"
-PRESET_VALUE = 0xFFFF
 POLYNOMAIL = 0x8408
 
+
 def crc(data):
-    val = PRESET_VALUE
+    val = 0xFFFF # PRESET_VALUE
     for i in xrange(len(data)):
         val ^= data[i]
         for j in xrange(8):
             if (val & 0x1):
-                val = (val >> 1) ^ POLYNOMIAL
+                val = (val >> 1) ^ 0x8408 # POLYNOMIAL value
             else:
                 val = val >> 1
     return val
     
 # Command - int
 # Data - List of bytes
-def sendCommand(command, data):
-    msg_data = [1,1].append(command)
+def sendCommand(ser, command, data):
+    msg_data = [len(data) + 4,0xFF]
+    msg_data.append(command)
     msg_data += data
     crc_val = crc(msg_data)
-    msg_data += crc_val & 0xFF
-    msg_data += (crc_val >> 8) & 0xFF
+    msg_data.append(crc_val & 0xFF)
+    msg_data.append((crc_val >> 8) & 0xFF)
     data_str = ""
     for byte in msg_data:
+        print(hex(byte))
         data_str += chr(byte)
 
     ser.write(data_str)
-    
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',
@@ -47,9 +47,17 @@ ser = serial.Serial(
     timeout=1
 )
 
-
-
-while(1):
+sendCommand(ser, 0x33, [0, 0xFF, 0xFF])
+print('\n')
+a = 1
+while (a > 0):
     x = ser.readline(TAG_LENGTH)
     if x:
-        print(str(x.encode('hex')))
+        x = x.encode('hex')
+        s = ""
+        for i in xrange(len(x)):
+            s += x[i]
+            if (i % 2 == 1):
+                s += ' '
+        print(s)
+        a -= 1
