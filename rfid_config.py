32538a9640cdd69d714f623a15bb881010853ddf
soctar <soctar@gmail.com>
2017-04-05 22:20:14 -0400

    Added wrapper functions for common commands
    Issue: Inventory multiple has arbitrary length data response,
           # of lines to read depends on len (first byte)

--- rfid_config.py
+++ rfid_config.py
@@ -1,27 +1,15 @@
 #! /usr/bin/env python
 
 # TODO
-# 
+#
 # LEN(1), ADDR(1), COMMAND(1), DATA(var), CRCLSB(1), CRCMSB(1)
 # Neil Ryan <nryan@andrew.cmu.edu>
 
-# Functions to send each type of command, waits for response in function
-# Include parsing of status/len/etc
-# Get Work command 0x36
-# Set work command 0x35
-# Acoustio-optic 0x33
-# Set scan time 0x25
-# Set power (with response)
-# Set address 0x24
-# Get reader info 0x21
-# Inventory single 6b 0x50
-# inventory multiple 0x51
-
 import serial
 
-TAG_LENGTH = 18
+LINE_LENGTH = 18
 
-
+# Perform a CRC on data (an array of integers)
 def crc(data):
     val = 0xFFFF # PRESET_VALUE
     for i in xrange(len(data)):
@@ -33,9 +21,29 @@ def crc(data):
                 val = val >> 1
     return val
 
-    
-# Command - int
-# Data - List of bytes
+
+# Flattens a 2D array into a 1D array
+def flatten(array):
+    return [val for sublist in array for val in sublist]
+
+
+# Takes a string read from serial, translates to array of data bytes
+def toBytes(data):
+    data = data.encode('hex')
+    data_bytes = []
+    byte = ""
+    for i in xrange(len(data)):
+        byte += data[i]
+        if (i % 2 == 1):
+            data_bytes.append(byte)
+            byte = ""
+    return data_bytes
+
+
+# Sends a command to a serial connection
+# ser: Serial connection (serial.Serial)
+# command: Integer command code
+# data: List of data bytes to send with command
 def sendCommand(ser, command, data):
     msg_data = [len(data) + 4,0x0]
     msg_data.append(command)
@@ -50,6 +58,112 @@ def sendCommand(ser, command, data):
 
     ser.write(data_str)
 
+# Recieves data from a serial connection
+# ser: Serial connection (serial.Serial)
+# num_lines: number of lines to recieve
+def recieveData(ser, num_lines):
+    data = []
+    while num_lines > 0:
+        x = ser.readline(LINE_LENGTH)
+        if x:
+            data.append(toBytes(x))
+            num_lines -= 1
+    return flatten(data)
+
+
+# Prints response data with some formatting
+# data: Array of data bytes (as strings) to print
+def printData(data):
+    assert(len(data) >= 6) # Len, Adr, reCmd, Status, CRCLSB, CRCMSB
+    data_len = len(data) - 4
+    print("Length: {}".format(int(data[0], 16))
+    print("Address: 0x{}".format(data[1]))
+    print("Response Command {}".format(data[2]))
+    print("Status: 0x{}".format(data[3]))
+
+    s = ""
+    num_bytes = 0
+    for i in xrange(4, data_len - 2):
+        s += "0x{} ".format(data[i])
+        num_bytes += 1
+        if num_bytes % 8 == 0:
+            s += "\n\t"
+
+    print("Data: {}".format(s))
+    print("CRC: 0x{}{}".format(data[data_len-1], data[data_len-2]))
+
+
+# Sets the power level of the reader
+def setPower(ser, power_level):
+    assert(power_level >= 0 && power_level <= 30)
+    sendCommand(ser, 0x2f, [power_level])
+    data = recieveData(ser, 1)
+    printData(data)
+
+# Get the work mode parameter of the reader
+def getWorkMode(ser):
+    sendCommand(ser, 0x21, [])
+    data = recieveData(ser, 2)
+    printData(data)
+
+# Set the address that the reader will listen at
+def setAddress(ser, address):
+    sendCommand(ser, 0x24, [address])
+    data = recieveData(ser, 1)
+    printData(data)
+
+# Set the InventoryScanTime of the reader
+def setScanTime(ser, scan_time):
+    sendCommand(ser, 0x25, [scan_time])
+    data = recieveData(ser, 1)
+    printData(data)
+
+# Set the LED light flash/buzzer tweet
+# activeT: LED flash and buzzer tweet time
+# silentT: LED and buzzer silent time
+# times: LED flash and buzzer tweet times
+def acoustoOpticControl(ser, activeT, silentT, times):
+    sendCommand(ser, 0x33, [activeT, silentT, times])
+    data = recieveData(ser, 1)
+    printData(data)
+
+
+# Set the reader information - see docs for definitions
+def setWorkMode(ser, read_mode, mode_state, mem_inven,
+        first_adr, word_num, tag_time):
+    sendCommand(ser, 0x35,
+            [read_mode, mode_state, mem_inven,
+                first_adr, word_num, tag_time])
+    data = recieveData(ser, 2)
+    printData(data)
+
+
+# Get the reader information
+def getWorkCommand(ser):
+    sendCommand(ser, 0x36, [])
+    data = recieveData(ser, 2)
+    printData(data)
+
+
+# Inventory one tag (6B) in effective field
+# If >1 tag in range, reader may get nothing
+def inventorySignal(ser):
+    sendCommand(ser, 0x50, [])
+    data = recieveData(ser, 1)
+    printData(data)
+
+
+# Inventory tags in effective field and get ID values
+# condition: Condition of detecting tags
+# addr: Tag's start address to compare
+# mask: data used to compare
+# wordData: array used to compare (8 bytes long)
+def inventoryMultiple(ser, condition, addr, mask, wordData):
+    wdata = flatten([condition, addr, mask, wordData])
+    sendCommand(ser, 0x51, wdata)
+    data = recieveData(ser, 5) # TODO Need to check based on length
+    printData(data)
+
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',
@@ -60,26 +174,7 @@ ser = serial.Serial(
     timeout=1
 )
 
-# Sets the power level of the reader
-def setPower(ser, power_level):
-    sendCommand(ser, 0x2f, [power_level])
-
-
 
 #sendCommand(ser, 0x35, [2, 0x2, 0x4, 0x0, 0x12, 0x0])
 sendCommand(ser, 0x36, []) #Gets work mode
 #sendCommand(ser, 0x21, [])
-
-print('')
-a = 2
-while (a > 0):
-    x = ser.readline(32)
-    if x:
-        x = x.encode('hex')
-        s = ""
-        for i in xrange(len(x)):
-            s += x[i]
-            if (i % 2 == 1):
-                s += ' '
-        print(s)
-        a -= 1
