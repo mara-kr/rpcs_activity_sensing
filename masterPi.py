e94856c6e0f785750690624c96e7370d9bad041d
aditichalis <aditi@chalisgaonkar.com>
2017-04-24 15:00:11 -0400

    Update masterPi.py

--- masterPi.py
+++ masterPi.py
@@ -1,3 +1,15 @@
-# MASTER PI CODE - SENDS DATA TO DATABASE
+#we get data from Matt
 
+
+#open file in json and parse
+
+
+#conflict resolutions
+
+#put data into csv
 import csv
+with open('master1.csv', 'a') as csvfile:
+    master1writer = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+    master1writer.writerow(['TagID'+ ';' + 'e/l'+ ';' + 'ReaderID' + ';' + 'time'])
+    
+# Send csv to database
