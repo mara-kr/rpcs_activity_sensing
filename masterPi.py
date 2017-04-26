ef1e1d691c30c612f5ccdc729edcea09f763831f
anapaolaap <apa@andrew.cmu.edu>
2017-04-26 15:12:02 -0400

    Update masterPi.py

--- masterPi.py
+++ masterPi.py
@@ -1,18 +1,23 @@
 
-#open file in json and parse
+#imports
 import json
+import requests
+import csv
 
 entry_ts = dict()
 
+#import json file and parse
 with open('data.text') as json_data:
     entry_ts = json.load(json_data)
 
 #conflict resolutions
 
 #put data into csv
-import csv
-with open('master1.csv', 'a') as csvfile:
-    master1writer = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
-    master1writer.writerow(['TagID'+ ';' + 'e/l'+ ';' + 'ReaderID' + ';' + 'time'])
+with open('master.csv', 'a') as csvfile:
+    masterwriter = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+    masterwriter.writerow(['TagID'+ ';' + 'e/l'+ ';' + 'ReaderID' + ';' + 'time'])
     
-# Send csv to database
+# Send csv to database - TEST THIS CODE
+url =  "http://128.237.182.83:8080"
+files = {'file': open('master.csv')}
+response = requests.post(url, files=files)
