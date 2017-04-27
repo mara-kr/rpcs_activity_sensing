82bf21ad15a46f37d95cbdb06ac3a03f6f1b1322
soctar <soctar@gmail.com>
2017-04-27 15:23:50 -0400

    Typo fixes to masterPi

--- masterPi.py
+++ masterPi.py
@@ -6,9 +6,9 @@ import os
 import datetime
 import requests
 
-DATA_FILE_DIR = "/home/pi/data"
-COMPOUND_DATA_FILE = "/home/pi/data/master_compound.csv"
-FILTERED_DATA_FILE = "/home/pi/data/master_filtered.csv"
+DATA_FILE_DIR = "/home/pi/data/"
+COMPOUND_DATA_FILE = DATA_FILE_DIR + "master_compound.csv"
+FILTERED_DATA_FILE = DATA_FILE_DIR + "master_filtered.csv"
 TIME_FORMAT = "%m/%d/%Y %H:%M:%S"
 TIME_THRESHOLD = 5  # Seconds between TODO, have entering/leaing
 POST_URL = "http://128.237.197.65:3000/sensors/reeive"
@@ -21,7 +21,7 @@ class DataLine:
         self.readerID = data[0]
         self.tagID = data[1]
         self.time = data[2]
-        self.datetime = datetime.strptime(self.time, TIME_FORMAT)
+        self.datetime = datetime.datetime.strptime(self.time, TIME_FORMAT)
         self.entering = data[3]
 
     def __lt__(self, other):
@@ -37,14 +37,16 @@ class DataLine:
 
 class DataFile:
     def __init__(self, fname):
-        self.fname = fname
-        self.handler = open(fname, 'r')
+        self.fname = DATA_FILE_DIR + fname
+        self.handler = open(self.fname, 'r')
         self.last_line = self.handler.readline()
 
     def newline(self):
         line = self.handler.readline()
-        self.last_line = DatLine(line)
-        return self.last_line
+        if (len(line) == 0):
+            return line
+        self.last_line = DataLine(line)
+        return line
 
 
 # Sort the data from multiple files by timestamp
@@ -65,10 +67,10 @@ def createCompoundFile():
                 min_line = data_files[i].last_line
                 min_i = i
 
-        out_f.write(str(min_lin))
+        out_f.write(str(min_line))
         if len(data_files[min_i].newline()) == 0:  # File is empty
             data_files[min_i].handler.close()
-            os.remove(data_files[min_i].fname)  # Cleanup file
+            #os.remove(data_files[min_i].fname)  # Cleanup file
             data_files.pop(min_i)
 
     out_f.close()  # Flush writes to disk
@@ -85,14 +87,14 @@ def filterCompoundFile():
     # Conflict detection
     for line in data_f:
         curr_line = DataLine(line)
-        for seen_line in history_bufer:
+        for seen_line in history_buffer:
             # Check for tag match in buffer - if so, remove both
-            if (seen_line.tagID == curr_line.tag and
+            if (seen_line.tagID == curr_line.tagID and
                     seen_line.readerID != curr_line.readerID):
                 history_buffer.remove(seen_line)
                 continue
             tdiff = curr_line.datetime - seen_line.datetime
-            tdiff = int(diff.total_seconds())
+            tdiff = int(tdiff.total_seconds())
             # When data outside range, write to file
             if (tdiff > TIME_THRESHOLD):
                 out_f.write(str(seen_line))
@@ -109,5 +111,7 @@ if __name__ == "__main__":
 
     # Send the data to the server
     f = open(FILTERED_DATA_FILE, 'rb')
-    r = requsts.post(POST_URL, files={'filtered_data.csv': f})
+    r = requests.post(POST_URL, files={'filtered_data.csv': f})
     f.close()
+    #os.remove(FILTERED_DATA_FILE)
+    #os.remove(COMPOUND_DATA_FILE)
